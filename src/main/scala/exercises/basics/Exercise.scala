312a14090fec95fef46bac9d7f8606400e067955
oknaidu <oknaidu@gmail.com>
2020-07-21 19:04:47 +0530

    Update Exercise.scala
    added implementation for excercise.

--- src/main/scala/exercises/basics/Exercise.scala
+++ src/main/scala/exercises/basics/Exercise.scala
@@ -5,16 +5,43 @@ object Exercise extends App {
   /**
    * 1. A greeting function (name, age) => "Hi , my name is $name and my age is $age"
    */
-  def greeting(name: String, age: Int): String = ???
+  def greeting(name: String, age: Int): String = {
+    s"Hi, my name is $name and my age is $age"
+  }
+
 
   /**
    * 2. A factorial function
    */
-  def factorial(n: Int): Int = ???
+  def factorial(n: Int, result: Int = 1): Int = {
+    if (n == 1)
+      return result;
+    else
+      factorial(n - 1, result * n)
+  }
 
 
   /**
    * 3. Test if a number is prime
-    */
-  def isPrime(n: Int): Boolean = ???
+   */
+  def isPrime(n: Int, diviser: Int = 2): Boolean = {
+    if (n == 0 || n == 1)
+      return false;
+    else if (n == 2)
+      return true;
+    else {
+      if (diviser > n / 2)
+        return true;
+      if (n % diviser == 0)
+        return false;
+      else
+        return isPrime(n, diviser + 1)
+    }
+
+
+  }
+
+  println(greeting("kumar", 10))
+  println(factorial(5))
+  println(isPrime(11))
 }
